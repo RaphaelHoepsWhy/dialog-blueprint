6698b158c64510d034b50a51f360e92f6a8cec79
RaphaelHoepsWhy <raphael.hoeps@why.de>
2025-02-06 12:13:04 +0100

    feat: WIP

--- app/Content.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-type Props = {
-  className?: string
-}
-
-export default function Content({}: Props) {
-  return <div>This is some content to be shown in the overlay</div>
-}
--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,7 @@
-import { DialogOrDrawer } from "@/components/DialogOrDrawer"
-import Content from "./Content"
-import { Maximize } from "lucide-react"
-
 export default function Home() {
   return (
     <div className="flex h-screen items-center justify-center p-4">
-      <DialogOrDrawer
-        trigger={
-          <button className="flex items-center justify-between gap-1 rounded-full border bg-white px-4 py-2 text-black">
-            <Maximize className="size-4" />
-            <span className="flex items-center gap-2">
-              <span className="font-semibold">{"Open"}</span>
-            </span>
-          </button>
-        }
-        title={"Overlay Content"}
-        description={"Add a short description here"}
-      >
-        <Content />
-      </DialogOrDrawer>
+      <h2>hello</h2>
     </div>
   )
 }
